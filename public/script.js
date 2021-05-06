266f8ee8deba6b3541986453ca0d6fa62e3d8c37
Sagar kumar jha <jha8768@gmail.com>
2021-05-06 08:01:16 +0530

    Video calling feature added

---
+++ public/script.js
@@ -0,0 +1,111 @@
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+const myPeer = new Peer(undefined, {
+	host: "peerjs-server.herokuapp.com",
+	secure: true,
+	port: 443,
+});
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+const peers = {};
+let myVideoStream;
+navigator.mediaDevices
+	.getUserMedia({
+		video: true,
+		audio: true,
+	})
+	.then((stream) => {
+		myVideoStream = stream;
+		addVideoStream(myVideo, stream);
+
+		myPeer.on("call", (call) => {
+			call.answer(stream);
+			const video = document.createElement("video");
+			call.on("stream", (userVideoStream) => {
+				addVideoStream(video, userVideoStream);
+			});
+		});
+
+		socket.on("user-connected", (userId) => {
+			connectToNewUser(userId, stream);
+		});
+	});
+
+socket.on("user-disconnected", (userId) => {
+	if (peers[userId]) peers[userId].close();
+});
+
+myPeer.on("open", (id) => {
+	socket.emit("join-room", ROOM_ID, id);
+});
+
+function connectToNewUser(userId, stream) {
+	const call = myPeer.call(userId, stream);
+	const video = document.createElement("video");
+	call.on("stream", (userVideoStream) => {
+		addVideoStream(video, userVideoStream);
+	});
+	call.on("close", () => {
+		video.remove();
+	});
+
+	peers[userId] = call;
+}
+
+function addVideoStream(video, stream) {
+	video.srcObject = stream;
+	video.addEventListener("loadedmetadata", () => {
+		video.play();
+	});
+	videoGrid.append(video);
+}
+
+const muteUnmute = () => {
+	const enabled = myVideoStream.getAudioTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getAudioTracks()[0].enabled = false;
+		setUnmuteButton();
+	} else {
+		setMuteButton();
+		myVideoStream.getAudioTracks()[0].enabled = true;
+	}
+};
+
+const playStop = () => {
+	let enabled = myVideoStream.getVideoTracks()[0].enabled;
+	if (enabled) {
+		myVideoStream.getVideoTracks()[0].enabled = false;
+		setPlayVideo();
+	} else {
+		setStopVideo();
+		myVideoStream.getVideoTracks()[0].enabled = true;
+	}
+};
+
+const setMuteButton = () => {
+	const html = `
+      <i class="fas fa-microphone"></i>
+    `;
+	document.querySelector("#muteButton").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+	const html = `
+      <i class="unmute fas fa-microphone-slash"></i>
+    `;
+	document.querySelector("#muteButton").innerHTML = html;
+};
+
+const setStopVideo = () => {
+	const html = `
+    <i class="fa fa-video-camera"></i>
+    `;
+	document.querySelector("#stopVideo").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+	const html = `
+    <i class="fas fa-video-slash"></i>
+    `;
+	document.querySelector("#stopVideo").innerHTML = html;
+};
